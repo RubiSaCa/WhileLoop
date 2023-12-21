7c58c31afe7212fe66ac43e28afa84af2016afef
Rubí <rubi_salcedocas@outlook.es>
2023-12-21 00:04:52 -0700

    New

---
+++ src/WhileLoop.java
@@ -0,0 +1,25 @@
+public class WhileLoop {
+
+    static boolean isTurnOnLight = false;
+
+    public static void main(String[] args){
+
+        turnOnOffLight();
+        
+        int i = 1;
+        while(isTurnOnLight && i<=10){
+            printSOS();
+            i++;
+        }
+
+    }
+    public static boolean turnOnOffLight(){
+        isTurnOnLight = (isTurnOnLight)?false:true;
+        return isTurnOnLight;
+    }
+
+    public static void printSOS(){
+        System.out.println(". . . _ _ _ . . .");
+    }
+}
+    
